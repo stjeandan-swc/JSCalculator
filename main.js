aaec6806785375e3ceb762ea8b37ca3ab19527be
stjeandan-swc <stjean.dan.dev@gmail.com>
2020-04-10 18:52:57 -0400

    Refactored code

--- main.js
+++ main.js
@@ -1,52 +1,35 @@
-window.onload = checkForNum;
-
-let isReset;
+let isReset = false;
 
 function insertValue(num) {
 
-
-  if(isReset && num != '+' && isReset && num != '-' &&
-      isReset && num != '*' && isReset && num != '/') {
-        clearCalc();
-        document.form.textview.value = document.form.textview.value + num;
-        checkForNum()
+  if((isReset && num != "+") && (isReset && num != "-") && (isReset && num != "*") && (isReset && num != "/")) {
+    clearCalc();
+    document.form.textview.value = document.form.textview.value + num;
   }
   else {
     isReset = false;
     document.form.textview.value = document.form.textview.value + num;
-    checkForNum()
   }
-
 }
 
 function clearCalc() {
   document.form.textview.value = "";
   isReset = false;
-  checkForNum()
 }
 
 function calculate() {
   let exp = document.form.textview.value;
 
-  document.form.textview.value = eval(exp);
-  isReset = true;
-}
+  if(exp.length < 1) { return; }
 
-function checkForNum() {
-  let testValue = document.form.textview.value;
-
-  if(testValue == "") {
-    document.getElementById('addition').disabled = true;
-    document.getElementById('subract').disabled = true;
-    document.getElementById('multipy').disabled = true;
-    document.getElementById('divide').disabled = true;
-    document.getElementById('eval').disabled = true;
+  try {
+    document.form.textview.value = eval(exp);
+    isReset = true;
   }
-  else {
-    document.getElementById('addition').disabled = false;
-    document.getElementById('subract').disabled = false;
-    document.getElementById('multipy').disabled = false;
-    document.getElementById('divide').disabled = false;
-    document.getElementById('eval').disabled = false;
+  catch(err) {
+    document.form.textview.value = "ERROR";
+    setTimeout(function() {
+      clearCalc();
+    }, 1000);
   }
 }
